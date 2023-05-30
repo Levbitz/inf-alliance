6c1e49a6615bbee2903b3e32ba042c7735cf512d
levbitz <maxinosamx@gmail.com>
2023-05-30 13:56:51 +0530

    Infinitz Alliance

--- src/components/Maintenance/Maintenance.jsx
+++ src/components/Maintenance/Maintenance.jsx
@@ -7,7 +7,7 @@ function Maintenance() {
     
     <div className="row">
     <h5>Marine Maintenance Services</h5>
-    <p>Kayex SA offers a suit of overhaul capabilities for marine hydraulic pump and rotating equipment repair including hydraulic marine repairs that can be performed both in-house and on-site. Our Technicians can work on any rotating equipment, pumps, cranes, and winches, domestic or foreign.</p>
+    <p> Infinitz Alliance  offers a suit of overhaul capabilities for marine hydraulic pump and rotating equipment repair including hydraulic marine repairs that can be performed both in-house and on-site. Our Technicians can work on any rotating equipment, pumps, cranes, and winches, domestic or foreign.</p>
 
     <p>We offer pump and rotating equipment repair services to marine merchant vessels, commercial and industrial ships, tug barges, and more.</p>
 
