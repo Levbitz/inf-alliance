65fb11fb3531861ee1dd7f6e1a281e716e27302e
levbitz <maxinosamx@gmail.com>
2023-05-31 16:02:43 +0530

    djkfjkdf

--- src/components/MarineOne/MarinOne.jsx
+++ src/components/MarineOne/MarinOne.jsx
@@ -1,21 +1,18 @@
 import React from 'react'
-import './MarinOne.css'
+import './MarinoOne.css'
 
 
 
 function MarinOne() {
   return (
-    <div className='marine_wrapper'>
+    <div className='marine_one_wrapper'>
     
     <div className="container">
     <div className="row">
     <div className="col l9 s12">
-    <h4 className='white-text'>Marine Solutions & Products</h4>
+    <h4 className='white-text'>Services</h4>
 
-    <p className='white-text'>Infinitz Alliance offers marine innovative solutions to all types of vessels worldwide. We provide services, repairs and an extensive range of outsourced marine parts that are designed to work within the toughest marine conditions.
-
-    </p>
-    <p className='white-text'>Check out our marine maintenance services offering below.</p>
+    
     </div>
     </div>
     </div>
